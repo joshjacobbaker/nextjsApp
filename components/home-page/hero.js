ab26e8310b6af745af86a44b97fd715a37057e53
Josh Baker <josh.jacob.baker@gmail.com>
2021-09-07 14:51:30 -0700

    first commit

---
+++ components/home-page/hero.js
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+import classes from "./hero.module.css"
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image src="/images/site/josh.png" alt="An image showing Max" width={300} height={300} />
+      </div>
+      <h1>Hi, I'm Josh</h1>
+      <p>I blog about web development - especially frontend frameworks like React.</p>
+    </section>
+  )
+}
+
+export default Hero
