8106168c505d5b7393aedada730c9e97e04f469e
Josh Baker <josh.jacob.baker@gmail.com>
2021-09-07 16:17:14 -0700

    2nd Commit

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
-        <Image src="/images/site/josh.png" alt="An image showing Max" width={300} height={300} />
+        <Image src="/images/site/josh.png" alt="An image showing Josh" width={300} height={300} />
       </div>
       <h1>Hi, I'm Josh</h1>
       <p>I blog about web development - especially frontend frameworks like React.</p>
